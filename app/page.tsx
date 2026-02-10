2577e2e361fa170da769e5171b907c4b1772df40
xndadelin <adelinmihai071@gmail.com>
2026-02-10 17:43:50 +0200

    Initialise project.

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen flex flex-col items-center justify-between p-24">
+      hi there! this is codeapex!
+    </main>
+  );
+}
