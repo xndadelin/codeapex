3bddfd6df70c63ce10d84144a0874ca6dedbb71a
xndadelin <adelinmihai071@gmail.com>
2026-04-03 19:46:26 +0300

    Create route for GET submissions per challenge.

---
+++ app/api/submissions/challenge/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server"
+import { createClient } from "@/utils/supabase/server"
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url)
+
+    const challengeId = searchParams.get("challenge_id")
+
+    if(!challengeId) {
+        return NextResponse.json({
+            error: "challenge_id is required"
+        }, {
+            status: 400
+        })
+    }
+
+    const supabase = await createClient()
+
+    const { data, error } = await supabase.from("submissions").select("*").eq("challenge_id", challengeId)
+
+    if(error) {
+        return NextResponse.json({
+            error: error.message
+        }, {
+            status: 500
+        })
+    }
+
+    return NextResponse.json(data)
+}
